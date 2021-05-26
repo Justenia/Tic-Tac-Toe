2f3f1a5b730f38dc3c6946da985d5fc4711203a3
Justenia Maire San Jose <sanjosejusteniamaire@gmail.com>
2021-05-26 12:05:07 +0800

    try code for checking winner

--- Tic-Tac-Toe/Form1.cs
+++ Tic-Tac-Toe/Form1.cs
@@ -30,7 +30,25 @@ namespace Tic_Tac_Toe
 
             turn = !turn;
             button.Enabled = false;
+
+            checkWinner();
         }
+
+        private void checkWinner()
+        {
+            bool there_is_a_winner = false;
+            //horizontal
+            if ((btnX1.Text == btnX2.Text) && (btnX2.Text == btnX3.Text))
+                there_is_a_winner = true;
+            MessageBox.Show("You Win!");
+            if ((btnY1.Text == btnY2.Text) && (btnY2.Text == btnY3.Text))
+                there_is_a_winner = true;
+            MessageBox.Show("You Win!");
+            if ((btnZ1.Text == btnZ2.Text) && (btnZ2.Text == btnZ3.Text))
+                there_is_a_winner = true;
+            MessageBox.Show("You Win!");
+        }
+
 
         private void btnNew_Click(object sender, EventArgs e)
         {
