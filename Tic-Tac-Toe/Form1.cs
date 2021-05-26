a96886795043cab39280aa7a30936c82496e7204
Justenia Maire San Jose <sanjosejusteniamaire@gmail.com>
2021-05-26 13:27:59 +0800

    declaring winner code
    I used else if to specify a new condition to test if the first condition is not true.  I also disabled buttons ( all buttons) to avoid message box to keep on popping.  Also, for us to be able to determine the winner.

--- Tic-Tac-Toe/Form1.cs
+++ Tic-Tac-Toe/Form1.cs
@@ -39,26 +39,47 @@ namespace Tic_Tac_Toe
             bool we_have_a_winner = false;
 
             //horizontal
-            if ((btnX1.Text == btnX2.Text) && (btnX2.Text == btnX3.Text))
+            if ((btnX1.Text == btnX2.Text) && (btnX2.Text == btnX3.Text) && (!btnX1.Enabled))
                 we_have_a_winner = true;
-            else if ((btnY1.Text == btnY2.Text) && (btnY2.Text == btnY3.Text))
+            else if ((btnY1.Text == btnY2.Text) && (btnY2.Text == btnY3.Text) && (!btnY1.Enabled))
                 we_have_a_winner = true;
-            else if ((btnZ1.Text == btnZ2.Text) && (btnZ2.Text == btnZ3.Text))
+            else if ((btnZ1.Text == btnZ2.Text) && (btnZ2.Text == btnZ3.Text) && (!btnZ1.Enabled))
                 we_have_a_winner = true;
 
             //vertical
-            if ((btnX1.Text == btnY1.Text) && (btnY1.Text == btnZ1.Text))
+            else if ((btnX1.Text == btnY1.Text) && (btnY1.Text == btnZ1.Text) && (!btnX1.Enabled))
                 we_have_a_winner = true;
-            else if ((btnX2.Text == btnY2.Text) && (btnY2.Text == btnZ2.Text))
+            else if ((btnX2.Text == btnY2.Text) && (btnY2.Text == btnZ2.Text) && (!btnX2.Enabled))
                 we_have_a_winner = true;
-            else if ((btnX3.Text == btnY3.Text) && (btnY3.Text == btnZ3.Text))
+            else if ((btnX3.Text == btnY3.Text) && (btnY3.Text == btnZ3.Text) && (!btnX3.Enabled))
                 we_have_a_winner = true;
 
             //diagonal
-            if ((btnX1.Text == btnY2.Text) && (btnY2.Text == btnZ3.Text))
+            else if ((btnX1.Text == btnY2.Text) && (btnY2.Text == btnZ3.Text) && (!btnX1.Enabled))
                 we_have_a_winner = true;
-            else if ((btnX3.Text == btnY2.Text) && (btnY2.Text == btnZ1.Text))
+            else if ((btnX3.Text == btnY2.Text) && (btnY2.Text == btnZ1.Text) && (!btnX3.Enabled))
                 we_have_a_winner = true;
+
+            if (we_have_a_winner)
+            {
+                disableButtons();
+
+                String Winner = "";
+                if (turn)
+                    Winner = "O";
+                else
+                    Winner = "X";
+
+                MessageBox.Show(Winner + " Victory Belongs to You!", "Winner");
+            }
+        }
+        private void disableButtons()
+        {
+            foreach (Control c in Controls)
+            {
+                Button button = (Button)c;
+                button.Enabled = false;
+            }
         }
 
 
